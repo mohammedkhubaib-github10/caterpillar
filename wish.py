da12d689f40df5f218812b5827204162c1bbfbd6
mohammedkhubaib-github10 <138954346+mohammedkhubaib-github10@users.noreply.github.com>
2024-09-08 21:29:48 +0530

    Update wish.py

--- wish.py
+++ wish.py
@@ -18,5 +18,5 @@ def wish_birthday():
     return jsonify({"message": birthday_message})
 
 if __name__ == '__main__':
-    port=int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0",port=port)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
