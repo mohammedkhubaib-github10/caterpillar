27ffd75b36536fc5673586306f46717b68c5a844
mohammedkhubaib-github10 <138954346+mohammedkhubaib-github10@users.noreply.github.com>
2024-09-15 14:27:03 +0530

    Update wish.py

--- wish.py
+++ wish.py
@@ -4,18 +4,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Define the API route
-@app.route('/wish',methods=['POST'])
-def wish_birthday():
-    # Get the name from the request body (JSON format)
-    data = request.json
-    name = data.get('name')  # Extract 'name' from the received JSON
 
+# Define the API route
+@app.route('/wish', methods=['GET'])
+def wish_birthday():
     # Generate the birthday message
-    birthday_message = f"Happy Birthday, {name}!"
+    message = ["Happy Birthday", "Happy Friendship day", "Happy Fathers day"]
+    messagedic = {}
+    mes=[]
+    for i in range(0, len(message)):
+        messagedic={f'message {i + 1}':message[i]}
+        mes.append(messagedic)
 
     # Return the message as JSON response
-    return jsonify({"message": birthday_message})
+    return jsonify(mes)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
